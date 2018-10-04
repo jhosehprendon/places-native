38709787225f1a04fc880c539c48b240e01e526f
Jhosehp <jhosehp.rendon@gmail.com>
2018-10-04 14:18:26 -0500

    Add Input to add places and render them on list component

---
+++ App.js
@@ -0,0 +1,39 @@
+
+import React, {Component} from 'react';
+import { View, StyleSheet } from 'react-native';
+import PlaceInput from './src/components/PlaceInput/PlaceInput'
+import PlaceList from './src/components/PlaceList/PlaceList'
+
+export default class App extends Component {
+  state = {
+    places: []
+  }
+
+  placeAddedHandler = placeName => {
+
+    this.setState(prevState => {
+      return {
+        places: prevState.places.concat(placeName)
+      };
+    });
+ };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <PlaceInput onPlaceAdded={this.placeAddedHandler} />
+        <PlaceList places={this.state.places}/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 26,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'flex-start'
+  }
+})
---
+++ src/components/PlaceInput/PlaceInput.js
@@ -0,0 +1,59 @@
+import React, {Component} from 'react';
+import { View, Button, TextInput, StyleSheet } from 'react-native';
+
+class PlaceInput extends Component {
+    state = {
+        placeName: ''
+      }
+    
+    placeNameChangeHandler = val => {
+        this.setState({
+          placeName: val
+        })
+      }
+
+    placeSubmitHandler = () => {
+        if(this.state.placeName.trim() === '') {
+          return;
+        }
+    
+        this.props.onPlaceAdded(this.state.placeName)
+     };
+
+    render() {
+        return(
+            <View style={styles.inputContainer}>
+                <TextInput 
+                style={styles.placeInput}
+                placeholder='Awesome place'
+                value={this.state.placeName} 
+                onChangeText={this.placeNameChangeHandler}
+                />
+                <Button 
+                style={styles.placeButton} 
+                title='Add' 
+                onPress={this.placeSubmitHandler}
+                />
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+
+    inputContainer: {
+      // flex: 1,
+      width: '85%',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center'
+    },
+    placeInput: {
+      width: '70%'
+    },
+    placeButton: {
+      width: '30%'
+    }
+  })
+
+export default PlaceInput;
